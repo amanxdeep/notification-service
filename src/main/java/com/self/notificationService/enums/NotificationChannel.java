c363db594e74c23f089a767af1a915701cea334d
amanxdeep <amanxdeep1@gmail.com>
2025-11-01 16:51:06 +0530

    Add NotificationChannel and NotificationProvider enums for channel and provider types

---
+++ src/main/java/com/self/notificationService/enums/NotificationChannel.java
@@ -0,0 +1,7 @@
+package com.self.notificationService.enums;
+
+public enum NotificationChannel {
+    EMAIL,
+    SMS,
+    WHATSAPP
+}
---
+++ src/main/java/com/self/notificationService/enums/NotificationProvider.java
@@ -0,0 +1,7 @@
+package com.self.notificationService.enums;
+
+public enum NotificationProvider {
+    AWS,
+    TWILIO,
+    POSTMARK
+}
