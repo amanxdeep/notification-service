4408b7e6b654af0adb93b56e82569aa041e4be33
amanxdeep <amanxdeep1@gmail.com>
2026-01-11 13:19:40 +0530

    feat: Enhance TwilioSmsProviderService with error logging for API exceptions

--- src/main/java/com/self/notificationService/provider/smsProvider/TwilioSmsProviderService.java
+++ src/main/java/com/self/notificationService/provider/smsProvider/TwilioSmsProviderService.java
@@ -7,12 +7,15 @@ import com.self.notificationService.model.dto.SmsDto;
 import com.self.notificationService.model.dto.request.NotificationRequest;
 import com.self.notificationService.model.dto.response.NotificationSendResult;
 import com.self.notificationService.provider.NotificationProviderService;
+import com.twilio.exception.ApiException;
 import com.twilio.rest.api.v2010.Account;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
 
+@Slf4j
 @Service
 public class TwilioSmsProviderService implements NotificationProviderService {
 
@@ -47,7 +50,11 @@ public class TwilioSmsProviderService implements NotificationProviderService {
         try {
             Account.fetcher().fetch();
             return true;
+        } catch (ApiException e) {
+            log.error("Twilio API error: {}", e.getMessage(), e);
+            return false;
         } catch (Exception e) {
+            log.error("Twilio not reachable: {}", e.getMessage(), e);
             return false;
         }
     }
